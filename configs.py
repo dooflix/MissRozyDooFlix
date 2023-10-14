90efd9ac888a56c1fb2f20ebbf4409cc69f8823e
dooflix <116429820+dooflix@users.noreply.github.com>
2023-10-15 00:51:34 +0500

    Update configs.py

--- configs.py
+++ configs.py
@@ -30,7 +30,7 @@ else:
     URL = "http://{}/".format(FQDN)
 UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
 BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001987654567")).split())) 
-STREAM_LOGS = environ.get('STREAM_LOGS','1001968394711')
+STREAM_LOGS = environ.get('STREAM_LOGS','-1001773665600')
 SESSION = environ.get('SESSION','MissRozy')
 CUSTOM_CAPTION = environ.get('CUSTOM_CAPTION')
 
@@ -44,7 +44,7 @@ class Config(object):
 	BOT_OWNER = int(os.environ.get("BOT_OWNER", "6188583231"))
 	DATABASE_URL = os.environ.get("DATABASE_URL","mongodb+srv://root:223190@cluster0.6xl6cmc.mongodb.net/?retryWrites=true&w=majority")
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001765107260")
-	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "1001968394711")
+	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001773665600")
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
